60c8d1fdab6c179b33328dbb3ff65743eaa20702
kuramapommel <pommel.pg@gmail.com>
2019-01-08 12:32:30 +0900

    EmptyIdentifier追加

--- src/main/scala/com/tiloom6/wannatag/domain/common/Identifier.scala
+++ src/main/scala/com/tiloom6/wannatag/domain/common/Identifier.scala
@@ -21,3 +21,25 @@ trait Identifier [+A] {
 
   override def hashCode: Int = 31 * value.##
 }
+
+/*
+ * 値のない識別子
+ */
+trait EmptyIdentifier extends Identifier[Nothing] {
+
+  def value: Nothing = throw new NoSuchElementException
+
+  override val isDefined: Boolean = false
+
+  override def equals(obj: Any) = obj match {
+    case that: EmptyIdentifier => this eq that
+    case _ => false
+  }
+
+  override def hashCode: Int = 0
+}
+
+/*
+ * 値のない識別子のコンパニオンオブジェクト
+ */
+object EmptyIdentifier extends EmptyIdentifier
