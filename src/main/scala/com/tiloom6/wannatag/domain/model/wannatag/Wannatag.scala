05bde40291313eb73b4039ee927623a99cdd1c40
kuramapommel <pommel.pg@gmail.com>
2019-01-11 03:03:08 +0900

    wannatag unapple

--- src/main/scala/com/tiloom6/wannatag/domain/model/wannatag/Wannatag.scala
+++ src/main/scala/com/tiloom6/wannatag/domain/model/wannatag/Wannatag.scala
@@ -37,19 +37,31 @@ trait Wannatag {
 object Wannatag {
 
   /*
-   * Wannatagファクトリメソッド
+   * Wannatag apply
    *
    * @param id [[WannatagId]]
    * @param title [[WannatagTitle]]
    * @param body [[WannatagBody]]
    * @param username [[WannatagAuthorName]]
    * @param postDate [[WannatagPostDate]]
+   *
+   * @return Wannatagエンティティ
    */
   def apply(id: WannatagId,
             title: WannatagTitle,
             body: WannatagBody,
             username: WannatagAuthorName,
             postDate: WannatagPostDate): Wannatag = WannatagImpl(id, title, body, username, postDate)
+
+  /*
+   * Wannatag unapply
+   *
+   * @param wannatag Wannatag
+   *
+   * @return (WannatagId, WannatagTitle, WannatagBody, WannatagAuthorName, WannatagPostDate) tuple
+   */
+  def unapply(wannatag: Wannatag): Option[(WannatagId, WannatagTitle, WannatagBody, WannatagAuthorName, WannatagPostDate)] =
+    Some(wannatag.id, wannatag.title, wannatag.body, wannatag.username, wannatag.postDate)
 }
 
 /*
