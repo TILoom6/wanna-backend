879328cf1b770918aab544cb59ec731563c8155d
kuramapommel <pommel.pg@gmail.com>
2019-01-24 23:59:15 +0900

    create wannatag by user.

--- src/main/scala/com/tiloom6/wannatag/domain/model/wannatag/package.scala
+++ src/main/scala/com/tiloom6/wannatag/domain/model/wannatag/package.scala
@@ -1,6 +1,7 @@
 package com.tiloom6.wannatag.domain.model.wannatag
 
-import com.tiloom6.wannatag.domain.model.user.UserId
+import com.tiloom6.wannatag.domain.model.{DomainResult, EntityIdGenerator}
+import com.tiloom6.wannatag.domain.model.user.{User, UserId}
 
 /*
  * [[com.tiloom6.wannatag.domain.model.wannatag.Wannatag]] の暗黙定義パッケージ
@@ -19,5 +20,19 @@ package object implicits {
      * @return [[com.tiloom6.wannatag.domain.model.wannatag.WannatagAuthorId]]
      */
     implicit def UserIdToWannatagAuthorId(userId: UserId) = WannatagAuthorId(userId.value)
+  }
+
+  object Extension {
+    implicit case class Author(user: User) {
+      def createWannatag(title: WannatagTitle, body: WannatagBody, postDate: WannatagPostDate)(implicit idGenerator: EntityIdGenerator[WannatagId]): DomainResult[Wannatag, _] = {
+        val wannatag = Wannatag(
+          id = idGenerator.generate(),
+          title = title,
+          body = body,
+          authorId = user.id,
+          postDate = postDate
+        )
+      }
+    }
   }
 }
