1ac3b5339f9730baafb55c186095f7009967d0c4
kuramapommel <pommel.pg@gmail.com>
2019-01-25 20:08:04 +0900

    サーバ待機を外でやる

--- src/main/scala/com/tiloom6/wannatag/Server.scala
+++ src/main/scala/com/tiloom6/wannatag/Server.scala
@@ -5,29 +5,55 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.Http
 import akka.stream.Materializer
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.io.StdIn
 
+/*
+ * サーバトレイト
+ */
 trait Server {
+
+  /*
+   * APIルート
+   */
   protected val route: Route
 
+  /*
+   * ホスト
+   */
   protected val host: String
 
+  /*
+   * ポート番号
+   */
   protected val port: Int
 
+  /*
+   * ActorSystem
+   */
   protected implicit val actorSystem: ActorSystem
 
+  /*
+   * Materializer
+   */
   protected implicit val materializer: Materializer
 
+  /*
+   * ExecutionContext
+   */
   protected implicit val executionContext: ExecutionContext
 
-  def start() => {
+  /*
+   * サーバ起動
+   *
+   * @return 起動したサーバ
+   */
+  def start(): Future[Http.ServerBinding] = {
+    // サーバ起動
     val server = Http().bindAndHandle(route, host, port)
-
     println(s"Server online at host = $host, port = $port. Press RETURN to stop...")
 
-    // enter押下でサーバ停止
-    StdIn.readLine()
     server.flatMap(_.unbind()).onComplete(_ => actorSystem.terminate())
+    server
   }
 }
