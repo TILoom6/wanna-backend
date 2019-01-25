d33a7cd3184756b90ba4d3af4701be2f6a7b5897
kuramapommel <pommel.pg@gmail.com>
2019-01-25 12:42:59 +0900

    WannatagRepository

---
+++ src/main/scala/com/tiloom6/wannatag/domain/repository/wannatag/WannatagRepository.scala
@@ -0,0 +1,38 @@
+package com.tiloom6.wannatag.domain.repository.wannatag
+
+import com.tiloom6.wannatag.domain.model.wannatag.{Wannatag, WannatagId}
+import org.joda.time.DateTime
+
+import scala.concurrent.Future
+
+/*
+ * [[com.tiloom6.wannatag.domain.model.wannatag.Wannatag]] のリポジトリトレイト
+ */
+trait WannatagRepository {
+
+  /*
+   * 条件を満たすWannatagリストを取得する
+   *
+   * @param criterionPostDate 取得基準とするWannatag投稿日 TODO ValueObject定義する
+   * @param compare criterionPostDateより新しいものを取得するか古いものを取得するか TODO ValueObject定義する
+   * @param limit 取得件数 TODO ValueObject定義する
+   * @return Wannatagリストを取得するFuture
+   */
+  def getWannatagsThatSatisfyCondition(criterionPostDate: DateTime, isOlder: Boolean = true, limit: Long = -1L): Future[Option[Seq[Wannatag]]]
+
+  /*
+   * Wannatagを保存（Insert or Update）する
+   *
+   * @param wannatag 保存対象のWannatag
+   * @return 保存したWannatag
+   */
+  def save(wannatag: Wannatag): Future[Wannatag]
+
+  /*
+   * Wannatagを削除する
+   *
+   * @param id 削除対象の [[com.tiloom6.wannatag.domain.model.wannatag.WannatagId]]
+   * @return 削除したWannatag
+   */
+  def delete(id: WannatagId): Future[Wannatag]
+}
